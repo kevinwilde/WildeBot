e652430b5415d025c682342ff8dbea91f4bad401
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-06-22 20:46:23 -0400

    Go back to movie reviews bayes bc Twitter causes app to exceed memory

--- globalvars.py
+++ globalvars.py
@@ -1,8 +1,8 @@
-import bayes
+import bayesbest
 
 PAT = '***REMOVED***'
 PASSWORD = 'my_voice_is_my_password_verify_me'
 
-b = bayes.Bayes_Classifier()
+b = bayesbest.Bayes_Classifier()
 TTT_EXTENSION = "TTTGame.pickle"
 MANCALA_EXTENSION = "MancGame.pickle"
