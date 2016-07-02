dc1dfc4a0eca4cde70436df5930bd21a689eda45
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-07-01 21:45:59 -0400

    Send message is a method of bot class

--- bot.py
+++ bot.py
@@ -106,96 +106,96 @@ class Bot(object):
         """Tic Tac Toe game hosted on server"""
         if tokens[1].lower() == "new":
             t = games.TicTacToe.TTTBoard()
-            send_message(self.token, sender, str(t) + "\nYou go first")
+            self.send_message(sender, str(t) + "\nYou go first")
             t.save_game(sender + self.ttt_extension)
         else:
             try:
                 t = games.TicTacToe.TTTBoard.load_game(sender + self.ttt_extension)
                 move = int(tokens[1])
                 if t.game_over():
-                    send_message(self.token, sender, "Game over\nStart new game with TTT new")
+                    self.send_message(sender, "Game over\nStart new game with TTT new")
                 elif t.turn != 1:
-                    send_message(self.token, sender, "Not your turn yet")
+                    self.send_message(sender, "Not your turn yet")
                 elif t.legal_move(1, move):
                     t.make_move(1, move)
-                    send_message(self.token, sender, str(t))
+                    self.send_message(sender, str(t))
                     
                     if t.game_over():
                         if t.has_won(1):
-                            send_message(self.token, sender, "You win!")
+                            self.send_message(sender, "You win!")
                         else:
-                            send_message(self.token, sender, "Cat's game")
+                            self.send_message(sender, "Cat's game")
 
                     # Bot responds
                     else:
                         player2 = Player.Player(2, Player.ABPRUNE, ply=9)
                         ab_move = player2.choose_move(t)
                         t.make_move(2, ab_move)
-                        send_message(self.token, sender, str(t))
+                        self.send_message(sender, str(t))
                         
                         if t.game_over():
                             if t.has_won(2):
-                                send_message(self.token, sender, "I win!")
+                                self.send_message(sender, "I win!")
                             else:
-                                send_message(self.token, sender, "Cat's game")
+                                self.send_message(sender, "Cat's game")
                     
                     t.save_game(sender + self.ttt_extension)
                 else:
-                    send_message(self.token, sender, "Illegal move")
+                    self.send_message(sender, "Illegal move")
 
             except Exception as e:
                 print e
                 err_msg = "Something went wrong...\nMake sure you are choosing a valid square"
-                send_message(self.token, sender, err_msg)
+                self.send_message(sender, err_msg)
 
 
     def host_mancala_game(self, sender, tokens):
         """Mancala game hosted on server"""
         if tokens[1].lower() == "new":
             m = games.Mancala.MancalaBoard()
-            send_message(self.token, sender, str(m) + "\nYou go first")
+            self.send_message(sender, str(m) + "\nYou go first")
             m.save_game(sender + self.mancala_extension)
         else:
             try:
                 m = games.Mancala.MancalaBoard.load_game(sender + self.mancala_extension)
                 move = int(tokens[1])
                 if m.game_over():
-                    send_message(self.token, sender, "Game over\nStart new game with Mancala new")
+                    self.send_message(sender, "Game over\nStart new game with Mancala new")
                 elif m.turn != 1:
-                    send_message(self.token, sender, "Not your turn yet")
+                    self.send_message(sender, "Not your turn yet")
 
                 else:
                     if m.legal_move(1, move):
                         m.make_move(1, move)
-                        send_message(self.token, sender, str(m))
+                        self.send_message(sender, str(m))
                     else:
-                        send_message(self.token, sender, "Illegal move")
+                        self.send_message(sender, "Illegal move")
 
                 # Bot responds
                 while m.turn == 2  and not m.game_over():
-                    send_message(self.token, sender, "My turn")
+                    self.send_message(sender, "My turn")
                     player2 = Player.Player(2, Player.CUSTOM, ply=9)
                     ab_move = player2.choose_move(m)
-                    send_message(self.token, sender, "I choose " + str(ab_move))
+                    self.send_message(sender, "I choose " + str(ab_move))
                     m.make_move(2, ab_move)
-                    send_message(self.token, sender, str(m))
+                    self.send_message(sender, str(m))
 
                 if m.turn == 1:
-                    send_message(self.token, sender, "Your turn again")
+                    self.send_message(sender, "Your turn again")
                 elif m.game_over():
                     if m.has_won(1):
-                        send_message(self.token, sender, "You win!")
+                        self.send_message(sender, "You win!")
                     elif m.has_won(2):
-                        send_message(self.token, sender, "I win!")
+                        self.send_message(sender, "I win!")
                     else:
-                        send_message(self.token, sender, "Cat's game")
+                        self.send_message(sender, "Cat's game")
 
                 m.save_game(sender + self.mancala_extension)
 
             except Exception as e:
                 print e
                 err_msg = "Something went wrong...\nMake sure you are choosing a valid square"
-                send_message(self.token, sender, err_msg)
+                self.send_message(sender, err_msg)
 
 
 
