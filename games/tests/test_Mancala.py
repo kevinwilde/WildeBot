9e9acc86b64115017acff9e21b03e283300a9ebe
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-07-10 11:36:02 -0400

    Fix instance member vars in test cases

--- games/tests/test_Mancala.py
+++ games/tests/test_Mancala.py
@@ -10,15 +10,15 @@ class TestMancala(unittest.TestCase):
 
     def test_legal_moves_empty_board(self):
         m = MancalaBoard()
-        m.P1Cups = [0] * m.NCUPS
-        m.P2Cups = [0] * m.NCUPS
+        m.P1Cups = [0] * m.num_cups
+        m.P2Cups = [0] * m.num_cups
         self.assertEqual(m.legal_moves(1), [])
         self.assertEqual(m.legal_moves(2), [])
 
     def test_game_over(self):
         m = MancalaBoard()
-        m.P1Cups = [0] * m.NCUPS
-        m.P2Cups = [0] * m.NCUPS
+        m.P1Cups = [0] * m.num_cups
+        m.P2Cups = [0] * m.num_cups
         self.assertTrue(m.game_over())
 
     def test_not_game_over(self):
@@ -36,17 +36,17 @@ class TestMancala(unittest.TestCase):
         self.assertEqual(m.P1Cups, [4,4,0,5,5,5])
         self.assertEqual(m.P2Cups, [4,4,4,4,4,4])
         self.assertEqual(m.turn, 1)
-        self.assertEqual(m.scoreCups[0], 1)
-        self.assertEqual(m.scoreCups[1], 0)
+        self.assertEqual(m.score_cups[0], 1)
+        self.assertEqual(m.score_cups[1], 0)
         self.assertEqual(m.legal_moves(1), [1,2,4,5,6])
 
     def test_clear_board_at_end(self):
         m = MancalaBoard()
         m.P1Cups = [0,0,0,0,0,1]
-        m.scoreCups = [23, 0]
+        m.score_cups = [23, 0]
         m.make_move(1, 6)
-        self.assertEqual(m.P1Cups, [0] * m.NCUPS)
-        self.assertEqual(m.P2Cups, [0] * m.NCUPS)
+        self.assertEqual(m.P1Cups, [0] * m.num_cups)
+        self.assertEqual(m.P2Cups, [0] * m.num_cups)
         self.assertTrue(m.game_over())
 
     def test_series_of_moves(self):
